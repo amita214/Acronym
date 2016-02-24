7e1f37db4159239cc08cef44adf2be5d6a2e353a
Amita Pai <amitapai@amitas-macbook-pro.local>
2016-02-23 16:06:22 -0800

    .

---
+++ Acronyms/Source/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acronyms
+//
+//  Created by Amita Pai on 2/19/16.
+//  Copyright © 2016 Amita Pai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
